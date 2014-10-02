977e87b78f662884dec6cb0303a26e6494ffd78f
Junggu Lee <hyde1117@naver.com>
2014-10-02 21:14:21 +0900

    Runable rev.

---
+++ my_project/add_test.cpp
@@ -0,0 +1,11 @@
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(Add)
+{
+
+};
+
+TEST(Add, Create)
+{
+	FAIL("Fail me!");
+}
---
+++ my_project/compile.sh
@@ -0,0 +1,1 @@
+g++ AllTests.cpp add_test.cpp -I../cpputest/include/ -L../cpputest/lib/ -lCppUTEst
